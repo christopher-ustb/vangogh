d98c5652ca372f9572235ea1b252094f3c9ee3f9
Christopher.Wang <wangchao_ustb_2012@163.com>
2019-04-10 10:50:51 +0800

    django filter

--- photo/views.py
+++ photo/views.py
@@ -1,32 +1,19 @@
+from django_filters import rest_framework as filters
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from photo.serializers import PhotoSerializer
+
 from photo.models import Photo
-from vangogh import settings
-import os
+from photo.serializers import PhotoSerializer
+
+
+class PhotoFilter(filters.FilterSet):
+    datetime_original = filters.DateTimeFromToRangeFilter(field_name="datetime_original")
+
+    class Meta:
+        model = Photo
+        exclude = ["image_hash", "gps_longitudes", "gps_latitudes", "gmt_created"]
 
 
 class PhotoViewSet(viewsets.ModelViewSet):
-    queryset = Photo.objects.all()
+    queryset = Photo.objects.all().order_by("-datetime_original", "-gmt_created")
     serializer_class = PhotoSerializer
-
-    @action(detail=False, methods=['get'])
-    def scan_dir(self, request):
-        picture_dir = os.path.join(settings.PHOTO_BASIC_DIR, "Douban")
-        files = os.listdir(picture_dir)
-        for f in files:
-            statinfo = os.stat(os.path.join(picture_dir, f))
-            photo_path = "Douban/" + f
-            if Photo.objects.filter(path=photo_path).count() == 0:
-                photo = Photo(path=photo_path)
-                photo.save()
-            print(statinfo.st_size)
-        return Response({'status': 'scan'})
-
-
-def save_disk_photo_to_db(photo_path):
-    photo = Photo(path=photo_path)
-    # TODO generate thumbnail, read exif
-    photo.save()
-    return photo
+    filterset_class = PhotoFilter
